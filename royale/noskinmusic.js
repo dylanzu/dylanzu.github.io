c5e5a8c7c5b553d22a9ab2b62a98bdc5f5fa6607
dylanzu <54517167+dylanzu@users.noreply.github.com>
2019-08-25 22:54:51 -0400

    Add files via upload

---
+++ royale/noskinmusic.js
@@ -0,0 +1,7 @@
+print("Dylanzu Music Mod, Skin Music Disabled!");
+var LOBBY_MUSIC_URL = "https://dylanzu.github.io/royale/music/lobby.mp3";
+var MENU_MUSIC_URL = "https://dylanzu.github.io/royale/music/menu.mp3";
+var SKIN_MUSIC_URL = {
+    420: "if this works lmao"
+};
+print("loading url.js finished");
---
+++ royale/url.js
@@ -0,0 +1,23 @@
+print("Music Mod by Dylanzu");
+var LOBBY_MUSIC_URL = "https://dylanzu.github.io/royale/music/lobby.mp3";
+var MENU_MUSIC_URL = "https://dylanzu.github.io/royale/music/menu.mp3";
+var SKINCOUNT = 1;
+var SKIN_MUSIC_URL = {};
+(function() {
+    $.ajax({
+        url: "assets.json",
+        success: function (result) {
+            assetData = result;
+        },
+        async: false
+    });
+    if (assetData.skins.count != undefined)
+        SKINCOUNT=assetData.skins.count;
+    for (i in assetData.skins.properties) {
+        var prop = assetData.skins.properties;
+        var music = prop[i].music;
+        if (music != undefined)
+            SKIN_MUSIC_URL[prop[i].id] = music;
+    }
+})();
+print("loading url.js finished");
