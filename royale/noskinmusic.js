ff5a824d86bffc33cb8ba480b6057807264dac88
dylanzu <54517167+dylanzu@users.noreply.github.com>
2019-08-26 09:06:59 -0400

    Update noskinmusic.js

--- royale/noskinmusic.js
+++ royale/noskinmusic.js
@@ -1,4 +1,4 @@
-print("Dylanzu Music Mod, Skin Music Disabled!");
+print("Skin Music Disabled!");
 var LOBBY_MUSIC_URL = "https://dylanzu.github.io/royale/music/lobby.mp3";
 var MENU_MUSIC_URL = "https://dylanzu.github.io/royale/music/menu.mp3";
 var SKIN_MUSIC_URL = {
