2de2f1d29f354441baea2616261187d9d3c8b913
dylanzu <54517167+dylanzu@users.noreply.github.com>
2019-11-23 20:19:48 -0500

    obsolete js file
    Not supported anymore

--- royale/noskinmusic.js
+++
@@ -1,7 +0,0 @@
-print("Bruh Moment");
-var LOBBY_MUSIC_URL = "https://dylanzu.github.io/royale/music/lobby.mp3";
-var MENU_MUSIC_URL = "https://dylanzu.github.io/royale/music/menu.mp3";
-var SKIN_MUSIC_URL = {
-    420: "if this works lmao"
-};
-print("loading url.js finished");
